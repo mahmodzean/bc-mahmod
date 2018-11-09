f5ea7c9974f04f41999d86a6d08f09944438568d
mahmodzean <44239884+mahmodzean@users.noreply.github.com>
2018-11-09 14:33:48 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,8 +7,8 @@ client.on('message', message => {
     if (message.author.id === client.user.id) return;
     if (message.guild) {
    let embed = new Discord.RichEmbed()
-    let args = message.content.split(' ').slice(1).join(' ');
-if(message.content.split(' ')[0] == prefix + '!bc') {
+    let args = message.content.split('!').slice(1).join('!');
+if(message.content.split('!')[0] == prefix + '!bc') {
     if (!args[1]) {
 return;
 }
@@ -17,7 +17,7 @@ return;
             var bc = new Discord.RichEmbed()
             .addField(' » الرسالة : ', args)
             .setColor('#ff0000')
-            // m.send(`[${m}]`);
+            // m.send(`[!{m}]`);
             m.send(`!{m}`,{embed: bc});
         });
     }
